f7e997ed4920ecaa478270aea4c7a9f14e367e77
Jatin Kumar <jatin208015@gmail.com>
2024-12-11 16:29:07 +0530

    project dto

--- src/main/java/com/jatin/project_management/dto/ProjectDto.java
+++ src/main/java/com/jatin/project_management/dto/ProjectDto.java
@@ -1,9 +1,20 @@
 package com.jatin.project_management.dto;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.jatin.project_management.entity.Task;
+
+import lombok.Data;
+
 /**
  * ProjectDto
  */
+@Data
 public class ProjectDto {
 
-	
+	private String name;
+	private String description;
+	private List<Task> tasks;
+	private LocalDateTime dueDate = LocalDateTime.MAX;
 }
