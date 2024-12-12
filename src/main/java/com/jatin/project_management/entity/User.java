2e8a5088f3a66a7e53ddb4a7ac931009eeb1e371
Jatin Kumar <jatin208015@gmail.com>
2024-12-12 12:01:20 +0530

    table name conflict

--- src/main/java/com/jatin/project_management/entity/User.java
+++ src/main/java/com/jatin/project_management/entity/User.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Data;
 
@@ -18,6 +19,7 @@ import lombok.Data;
 @Data
 @Builder
 @Entity
+@Table(name = "app_user")
 public class User {
 
 	@Id
